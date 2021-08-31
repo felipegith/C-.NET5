bc377c8a1012bccbdfcf8e92d8a4863cb42aa399
felipegith <felipefknowx@gmail.com>
2021-08-31 15:00:15 -0300

    Calculadora com todas operaçẽos através da URL

--- NET5RESTAPI - Calculadora/NET5RESTAPI/Controllers/CalculatorController.cs
+++ NET5RESTAPI - Calculadora/NET5RESTAPI/Controllers/CalculatorController.cs
@@ -20,8 +20,10 @@ namespace NET5RESTAPI.Controllers
             _logger = logger;
         }
 
+                // SOMA
+        // Não podemos ter duas rotas iguais, senao terão o erro de ambiguidade.
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
@@ -31,6 +33,82 @@ namespace NET5RESTAPI.Controllers
             }
             return BadRequest("Invalid Input");
         }
+
+
+            // SUBTRAÇÃO
+        // Não podemos ter duas rotas iguais, senao terão o erro de ambiguidade.
+        [HttpGet("subtract/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtract(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+
+                var subtract = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(subtract.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
+            // MULTIPLICAÇÃO
+        // Não podemos ter duas rotas iguais, senao terão o erro de ambiguidade.
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+
+                var multiplicate = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(multiplicate.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
+
+        // Não podemos ter duas rotas iguais, senao terão o erro de ambiguidade.
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+
+                var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
+        // MÉDIA
+        // Não podemos ter duas rotas iguais, senao terão o erro de ambiguidade.
+        [HttpGet("media/{firstNumber}/{secondNumber}")]
+        public IActionResult Media(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+
+                var media = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(media.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
+            // RAIZ QUADRADA
+        // Não podemos ter duas rotas iguais, senao terão o erro de ambiguidade.
+        [HttpGet("raiz/{firstNumber}")]
+        public IActionResult Raiz(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+
+                var raiz = Math.Sqrt((double)ConvertToDecimal(firstNumber));
+                return Ok(raiz.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
 
         private decimal ConvertToDecimal(string strNumber)
         {
